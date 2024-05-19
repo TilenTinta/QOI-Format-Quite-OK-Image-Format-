ed0e84b84e773e475da84f840e974dd5a9f96c2e
Tilen Tinta <titi.tinta1@gmail.com>
2024-05-19 10:37:56 +0200

    encoder works on RGB and RGBA

--- QOI_dekoder.py
+++ QOI_dekoder.py
@@ -6,39 +6,136 @@
 ###########################################################
 
 from PIL import Image
+import numpy as np
+import cv2
+
+
+def qoiHash(pixel):
+    # Hash je ostanek
+    return (pixel[0] * 3 + pixel[1] * 5 + pixel[2] * 7 + pixel[3] * 11) % 64
 
 
 if __name__ == "__main__":
 
+    # Oznake operacij
+    QOI_OP_RUN   = 0b11000000
+    QOI_OP_INDEX = 0b00000000
+    QOI_OP_DIFF  = 0b01000000
+    QOI_OP_LUMA  = 0b10000000 # 0x80
+    QOI_OP_RGB   = 0b11111110
+    QOI_OP_RGBA  = 0b11111111
+
+    QOI_op_maska = 0b11000000 # maska za ukaz
+    QOI_op_data  = 0b00111111 # maska za podatke
+    QOI_df_red   = 0b00110000 # maska za razliko rdeče
+    QOI_df_green = 0b00001100 # maska za razliko zelene
+    QOI_df_blue  = 0b00000011 # maska za razliko modre
+    QOI_luma_dg  = 0b00111111 # maska za luma zeleno
+    QOI_luma_rg  = 0b11110000 # maska za luma rdečo
+    QOI_luma_bg  = 0b00001111 # maska za luma modro
+
     # Uvozi datoteko
-    with open('output.qoi', 'rb') as file:
+    file = 'output.qoi'
+    with open(file, 'rb') as file:
         qoiFile = file.read()
 
     # Header, end #
-    qoi_end_marker = b'\x00\x00\x00\x00\x00\x00\x00\x01'   
-
-    array = [(0, 0, 0, 0)] * 64 
-    pxOld = (0, 0, 0, 255)
-    zaporedje = 0
-    pixelEncoded = bytearray()
-    #velikost = (width * height)
-
     header = qoiFile[:14]
     name = header[:4]
-    width = header[4:8]
-    height = header[8:12]
-    channels = header[12]
-    colorspace = header[13]
-    print(int.from_bytes(width))
-    print(int.from_bytes(height))
-    #for i in range(velikost):
-    #    if i < 14:
+    if name != b"qoif":
+        print("Datoteka ni qoi formata!")
+        exit(0)
+    width = int.from_bytes(header[4:8])
+    height = int.from_bytes(header[8:12])
+    channels = int(header[12])
+    colorspace = int(header[13])
+
+    qoi_end_marker = b'\x00\x00\x00\x00\x00\x00\x00\x01'   
+
+    buffer = qoiFile[14:(len(qoiFile) - len(qoi_end_marker))] # buffer pixlov brez headerja in konca
+    bufferEnd = []
+
+    array = [[0, 0, 0, 0]] * 64 
+    px = [0, 0, 0, 255]
+    zaporedje = 0
+    pixelEncoded = bytearray()
+
+    # Dekodiranje
+    i = 0
+    while i < len(buffer):
+
+        byte = buffer[i]
+
+        # QOI_OP_RGB #
+        if byte == QOI_OP_RGB:
+            px[0] = buffer[i + 1]
+            px[1] = buffer[i + 2]
+            px[2] = buffer[i + 3]
+            px[3] = 255
+            bufferEnd.extend(px)
+            i += 4
         
+        # QOI_OP_RGBA #
+        elif byte == QOI_OP_RGBA:
+            px[0] = buffer[i + 1]
+            px[1] = buffer[i + 2]
+            px[2] = buffer[i + 3]
+            px[3] = buffer[i + 4]
+            bufferEnd.extend(px)
+            i += 5
 
-       
+        # QOI_OP_INDEX #
+        elif (QOI_op_maska & byte) == QOI_OP_INDEX:
+            index = (byte & QOI_op_data)
+            px = array[index]
+            bufferEnd.extend(px)
+            i += 1
 
-    # Končni file
-    #qoiOutput = qoi_header + pixelEncoded + qoi_end_marker
+        # QOI_OP_DIFF #
+        elif (QOI_op_maska & byte) == QOI_OP_DIFF:
+            data = (byte & QOI_op_data)
+            dr = ((data & QOI_df_red) >> 4) - 2
+            dg = ((data & QOI_df_green) >> 2) - 2
+            db = (data & QOI_df_blue) - 2
+            px[0] = (px[0] + dr) & 0b11111111
+            px[1] = (px[1] + dg) & 0b11111111
+            px[2] = (px[2] + db) & 0b11111111
+            bufferEnd.extend(px)
+            i += 1
+
+        # QOI_OP_LUMA #
+        elif (QOI_op_maska & byte) == QOI_OP_LUMA:
+            dg = (byte & QOI_luma_dg) - 32 # bias
+            byte2 = buffer[i + 1]
+            drdg = ((byte2 & QOI_luma_rg) >> 4) - 8
+            dbdg = (byte2 & QOI_luma_bg) - 8
+
+            dr = drdg + dg
+            db = dbdg + dg
+
+            px[0] = (px[0] + dr) & 0b11111111
+            px[1] = (px[1] + dg) & 0b11111111
+            px[2] = (px[2] + db) & 0b11111111
+            bufferEnd.extend(px)
+            i += 2
+
+        # QOI_OP_RUN #
+        elif (QOI_op_maska & byte) == QOI_OP_RUN:
+            dataNum = (byte & QOI_op_data) + 1 # bias
+            for j in range(dataNum):
+                bufferEnd.extend(px)
+            i += 1
+            
+
+        index = qoiHash(px)
+        array[index] = px
+
         
     # Shrani file
-    
+    pic = np.array(bufferEnd, dtype=np.uint8).reshape((height, width, 4))
+    image = Image.fromarray(pic, 'RGBA')
+
+    image.show()
+    image.save('output.png')
+
+
--- QOI_enkoder.py
+++ QOI_enkoder.py
@@ -7,19 +7,20 @@
 
 from PIL import Image
 
-def qoiHeader(width, height, mode): # 14 bajtov
-    # Magic # - prvi 4 bajti
-    magic = b'qoif'
-    
-    # Width, Height # - vsak po 4 bajte (8 bytov)
 
-    # Channels, Colorspace # - vsak po 1 byte (2 bajta)
+def qoiHeader(width, height, mode): # 14 bajtov
+    # Magic - prvi 4 bajti
+    magic = b'qoif' 
+
+    # Width, Height - vsak po 4 bajte (8 bytov)
+
+    # Channels, Colorspace - vsak po 1 byte (2 bajta)
     if mode == 'RGB':
         channels = 3
         colorspace = 1
     if mode == 'RGBA':
         channels = 4
-        colorspace = 1 # ali 0? vseeno za file ampak javi drugačnost
+        colorspace = 0 # ali 1? vseeno za file ampak javi drugačnost
 
     header = magic + int(width).to_bytes(4,'big') + int(height).to_bytes(4,'big') + channels.to_bytes(1,'big') + colorspace.to_bytes(1,'big')
     return header
@@ -27,55 +28,64 @@ def qoiHeader(width, height, mode): # 14 bajtov
 
 def qoiHash(pixel):
     # Hash je ostanek
-    if len(pixel) == 4:
-        hash = (pixel[0] * 3 + pixel[1] * 5 + pixel[2] * 7 + pixel[3] * 11) % 64
-    else:
-        hash = (pixel[0] * 3 + pixel[1] * 5 + pixel[2] * 7) % 64
-    return hash
+    return (pixel[0] * 3 + pixel[1] * 5 + pixel[2] * 7 + pixel[3] * 11) % 64
 
 if __name__ == "__main__":
 
     # Uvozi datoteko
-    image = Image.open('./qoi_test_images/kodim23.png')
+    image = Image.open('./qoi_test_images/wikipedia_008.png') # kodim23
 
     # Podatki iz datoteke
-    #format = image.format
-    #print(format)
+    #print(image.format)
     width, height = image.size
     mode = image.mode
     print(mode)
-    piksli = list(image.getdata()) # (r, g, b, a)
+    piksli = list(image.getdata()) # (r, g, b, "a")
 
     # Header, end #
     qoi_header = qoiHeader(width, height, mode)
     qoi_end_marker = b'\x00\x00\x00\x00\x00\x00\x00\x01'
     #print(header)    
 
-    array = [(0, 0, 0, 0)] * 64 
-    pxOld = (0, 0, 0, 255)
+
+    array = [[0, 0, 0, 0]] * 64 
+    pxOld = [0, 0, 0, 255]
     zaporedje = 0
     pixelEncoded = bytearray()
-    velikost = (width * height)
+    velikost = len(piksli)
 
-    for i in range(velikost):
+    # Oznake operacij
+    QOI_OP_RUN   = 0b11000000
+    QOI_OP_INDEX = 0b00000000
+    QOI_OP_DIFF  = 0b01000000
+    QOI_OP_LUMA  = 0b10000000 #0x80
+    QOI_OP_RGB   = 0b11111110
+    QOI_OP_RGBA  = 0b11111111
 
-        px = piksli[i] # Trenuten pixel
+    # Kodiranje
+    for i in range(0, velikost):
+
+        px = list(piksli[i]) # Trenuten pixel
+        if mode == 'RGB':
+            px.append(255)
+
         if px == pxOld:
             zaporedje += 1
             # QOI_OP_RUN # 
-            if zaporedje == 62 or i == (velikost-1): # če ni velikost -1 zadnje iteracije ne shrani
-                pixelEncoded.append(0b11000000 | zaporedje - 1) # -1 zaradi primera samo enega pixla
+            if zaporedje == 62 or i == (velikost - 1): # če ni (velikost - 1) zadnje iteracije ne shrani
+                pixelEncoded.append(QOI_OP_RUN | (zaporedje - 1)) # -1 zaradi primera samo enega pixla
                 zaporedje = 0
         else:
             # QOI_OP_RUN #
             if zaporedje > 0:
-                pixelEncoded.append(0b11000000 | zaporedje - 1) # -1 zaradi primera samo enega pixla
+                pixelEncoded.append(QOI_OP_RUN | (zaporedje - 1)) # -1 zaradi primera samo enega pixla
                 zaporedje = 0
 
-            # QOI_OP_INDEX #
             Index = qoiHash(px) # Računanje indeksa
+
+            # QOI_OP_INDEX #
             if array[Index] == px:
-                pixelEncoded.append(0b00000000 | Index)
+                pixelEncoded.append(QOI_OP_INDEX | Index)
             else:
                 array[Index] = px  
 
@@ -86,35 +96,35 @@ if __name__ == "__main__":
                     db = px[2] - pxOld[2]
                     dr_dg = dr - dg
                     db_dg = db - dg
-
+                    
             # QOI_OP_DIFF #
                     if (dr <= 1 and dr >= -2) and (dg <= 1 and dg >= -2) and (db <= 1 and db >= -2):
                         dr += 2
                         dg += 2
                         db += 2
-                        colorBiti = 0b01000000 | (dr << 4) | (dg << 2) | db
+                        colorBiti = QOI_OP_DIFF | (dr << 4) | (dg << 2) | db
                         pixelEncoded.append(colorBiti)
-
+                        
             # QOI_OP_LUMA #    
                     elif (dg >= -32 and dg <= 31) and (dr_dg >= -8 and dr_dg <= 7) and (db_dg >= -8 and db_dg <= 7):
                         dg += 32
                         dr_dg += 8
                         db_dg += 8 
-                        pixelEncoded.append(0x80 | dg) 
+                        pixelEncoded.append(QOI_OP_LUMA | dg) 
                         pixelEncoded.append((dr_dg << 4) | db_dg)
                     else:
             # QOI_OP_RGB #
-                        pixelEncoded.append(0b11111110)
+                        pixelEncoded.append(QOI_OP_RGB)
                         pixelEncoded.append(px[0])
                         pixelEncoded.append(px[1])
                         pixelEncoded.append(px[2])
                 else:
             # QOI_OP_RGBA #
-                    pixelEncoded.append(0b11111111)
-                    pixelEncoded.append(px[0])
-                    pixelEncoded.append(px[1])
-                    pixelEncoded.append(px[2])
-                    pixelEncoded.append(px[3])
+                    pixelEncoded.append(QOI_OP_RGBA)
+                    pixelEncoded.extend(px)
+                    #pixelEncoded.append(px[1])
+                    #pixelEncoded.append(px[2])
+                    #pixelEncoded.append(px[3])
 
         pxOld = px
 
